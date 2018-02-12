2da1053255d1e7da9f5771590d8a4ecb216030e0
Ilya Bilyk <illyusha7bilyk@gmail.com>
2018-02-12 11:57:28 +0200

    INO button led control
    LED control with button.

---
+++ Arduino_INO/button_diode_control/button_diode_control.ino
@@ -0,0 +1,18 @@
+void setup() {
+  pinMode(A5, INPUT_PULLUP);
+  pinMode(12, INPUT);
+  pinMode(13, OUTPUT);
+
+  Serial.begin(9600);
+}
+
+void loop() {
+  boolean button1 = !digitalRead(A5);
+  boolean button2 = digitalRead(13);
+
+  if (button1 == 1) digitalWrite(13, 1);
+  else digitalWrite (13, 0);
+
+  Serial.print("Button 1: "); Serial.print(button1);
+  Serial.print(", Button 2: "); Serial.println(button2);
+}
